342fd7fd5fb2d762680ab88298c0e55c07bf51e8
Paul Anthony McGowan <info@xhostcom.com>
2022-09-14 19:16:43 +0100

    Update screenshots

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @consultations = Consultation.where(
+      start_time: Time.now.beginning_of_month.beginning_of_week..Time.now.end_of_month.end_of_week
+    )
+  end
 
   def terms; end
 
